6bd48e31086ff14328ec715c0809542980928784
Brayan Hamllelo Estevem Prado Marroquín <2995454660101@ingenieria.usac.edu.gt>
2021-05-03 14:55:47 -0600

    Mostrar el archivo de entrada

--- Main.py
+++ Main.py
@@ -1,26 +1,41 @@
 from flask import Flask, redirect, render_template, request, Response
 from flask_cors import CORS
+import base64
+from fronted.web import EntradasXML as xml
 
+salida=None
 app = Flask(__name__)
 cors = CORS(app, resourse={r"/*": {"origin": "*"}})
 
-@app.route('/events/', methods=['POST'])
-def post_events():
-    data = open('data.txt', 'w+')
-    data.write(request.data.decode('utf-8'))
-    data.close()
+@app.route('/')
+def init():
+    return redirect('inicio')
 
-    return Response(response=request.data.decode('utf-8'),
-                    mimetype='text/plain',
-                    content_type='text/plain')
+@app.route('/inicio')
+def inicio():
+    return render_template('Inicio.html')
 
-@app.route('/events/', methods=['GET'])
-def get_evets():
-    data = open('data.txt', 'r+')
-    return Response(response=data.read(),
+@app.route('/abrirArchivo', methods=['POST'])
+def abrirArchivo():
+    global salida
+    salida=""
+    datos = request.get_json()
+    if datos['data'] == '':
+        return {"msg": 'Error en contenido'}
+    contenido = base64.b64decode(datos['data']).decode('utf-8')
+    salida=contenido
+    print(salida)
+    #xml.AbrirArchivo(salida)
+    return salida
+
+@app.route('/abrirArchivo', methods=['GET'])
+def get_events():
+    global salida
+    data = salida
+    return Response(response=data,
                     mimetype='text/plain',
                     content_type='text/plain')
 
 if __name__ == '__main__':
     #app.run(threaded=True,port=5000)
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
